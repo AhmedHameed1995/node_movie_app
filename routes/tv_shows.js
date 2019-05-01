09197242cfe4ec30d2bba100cc695958de9a0455
Dev Team <ahmed.phpdeveloper@gmail.com>
2019-05-02 01:55:01 +0500

    Initial Commit

---
+++ routes/tv_shows.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const request = require("request");
+const router = express();
+const {GET_POPULAR_TV_SHOWS, GET_POSTER_PATH} = require('../config/api');
+
+router.get('/', (req, res) => {
+    request(GET_POPULAR_TV_SHOWS, (error,response) => {
+        const parseResults = JSON.parse(response.body);
+        console.log(parseResults);
+        res.render("tv_shows", {list_tv_shows:parseResults.results, poster_path:GET_POSTER_PATH});
+    })
+})
+
+module.exports = router;
